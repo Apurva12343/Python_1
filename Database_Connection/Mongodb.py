6556da09c3265ba9e9af6164db74f990d11f814c
Apurva12343 <apurvaaryan2222@gmail.com>
2024-12-15 00:53:16 +0530

    12:53

---
+++ Database_Connection/Mongodb.py
@@ -0,0 +1,10 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://iamapurvaaryan:Apurva@cluster0.lzgwt20.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client.test
+print(db)
+db = client['ApurvaAryan']
+data = {"name":"Apurva","class":"Data Science","friend":"Anjali"}
+coll_Apurva = db["my_record"]
+coll_Apurva.insert_one(data)
+data1 = {"mail":"apurva.aryan@outlook.com","phonenumber":"7366018523"}
+coll_Apurva.insert_one(data1)
