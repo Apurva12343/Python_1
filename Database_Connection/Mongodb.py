5ac0676fc9c81c78e0b81dff93830e8c8ec2e0ce
Apurva12343 <apurvaaryan2222@gmail.com>
2024-12-15 01:13:25 +0530

    01:13

--- Database_Connection/Mongodb.py
+++ Database_Connection/Mongodb.py
@@ -8,3 +8,5 @@ coll_Apurva = db["my_record"]
 coll_Apurva.insert_one(data)
 data1 = {"mail":"apurva.aryan@outlook.com","phonenumber":"7366018523"}
 coll_Apurva.insert_one(data1)
+for i in coll_Apurva.find():
+    print(i)
