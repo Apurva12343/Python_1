a2c0a3f4d541287a834851a970cb2736883a5f21
Apurva12343 <apurvaaryan2222@gmail.com>
2024-12-17 01:35:02 +0530

    01:35

---
+++ web/Google_News.py
@@ -0,0 +1,25 @@
+from selenium import webdriver    
+from bs4 import BeautifulSoup 
+import requests
+ 
+# For using sleep function because selenium 
+# works only when the all the elements of the 
+# page is loaded.
+import time 
+  
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By
+browser = webdriver.Chrome()
+browser.get('https://www.google.com')
+time.sleep(3)
+search_box = browser.find_element(By.CLASS_NAME,"gLFyf")
+search_box.send_keys("Crunchbase")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
+clickon = browser.find_element(By.LINK_TEXT,"News")
+clickon.click()
+time.sleep(3)
+news = browser.find_elements(By.CLASS_NAME,"SoAPf")
+file = open("news.pdf","w")
+for i in news:
+    file.write(i.text)
