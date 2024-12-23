fd374a470bac1a7d747607fa9d3b7a9fcfa23ce7
Apurva12343 <apurvaaryan2222@gmail.com>
2024-12-23 23:45:33 +0530

    23:45

--- Playwright/Playwright.py
+++ Playwright/Playwright.py
@@ -1,1 +1,12 @@
-import Playwright
+import re
+from playwright.sync_api import sync_playwright
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False,slow_mo=2000)
+    page = browser.new_page()
+    page.goto("https://playwright.dev/python/")
+    #click on a link by name of the link
+    docs = page.get_by_role("link",name="Docs")
+    docs.click()
+    #to get the url of the clicked link
+    print("docs: ",page.url)
+    browser.close()
